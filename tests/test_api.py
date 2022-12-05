2c0f3ac121c1b5ac09548e115d6b90da8bd9fe8a
mbroton <michal.broton.ns@gmail.com>
2022-12-05 20:24:06 +0100

    Update unit tests

--- tests/test_api.py
+++ tests/test_api.py
@@ -14,18 +14,3 @@ def test_api_with_invalid_session_key_ctx_manager():
     with pytest.raises(exceptions.InvalidResponseException):
         with api.ChatGPT(session_token="abc123"):
             pass
-
-
-def test_api_setting_up_conversation_id():
-    chat = api.ChatGPT(session_token="123", conversation_id="abc")
-    assert chat.conversation_id == "abc"
-
-
-def test_api_new_conversation_method():
-    chat = api.ChatGPT(session_token="foo", conversation_id="123")
-    chat.new_conversation()
-    assert chat.conversation_id is None
-
-
-def test_context_manager():
-    pass
