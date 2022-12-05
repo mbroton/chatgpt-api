9eea6f43323c7023926714ed25b311e5451ecc9e
mbroton <michal.broton.ns@gmail.com>
2022-12-05 17:14:08 +0100

    Modify unit tests according to latest changes

--- tests/test_api.py
+++ tests/test_api.py
@@ -5,17 +5,27 @@ from chatgpt import exceptions
 
 
 def test_api_with_invalid_session_key():
+    chat = api.ChatGPT(session_token="abc123")
     with pytest.raises(exceptions.InvalidResponseException):
-        with api.ChatGPT(session_token="abc123") as chat:
-            chat.authenticate()
+        chat.authenticate()
+
+
+def test_api_with_invalid_session_key_ctx_manager():
+    with pytest.raises(exceptions.InvalidResponseException):
+        with api.ChatGPT(session_token="abc123"):
+            pass
 
 
 def test_api_setting_up_conversation_id():
-    with api.ChatGPT(session_token="foo", conversation_id="123") as chat:
-        assert chat._conversation_id == "123"
+    chat = api.ChatGPT(session_token="123", conversation_id="abc")
+    assert chat.conversation_id == "abc"
 
 
 def test_api_new_conversation_method():
-    with api.ChatGPT(session_token="foo", conversation_id="123") as chat:
-        chat.new_conversation()
-        assert chat._conversation_id is None
+    chat = api.ChatGPT(session_token="foo", conversation_id="123")
+    chat.new_conversation()
+    assert chat.conversation_id is None
+
+
+def test_context_manager():
+    pass
